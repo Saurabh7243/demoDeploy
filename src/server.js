77a3b03a45754e0272206833e223eebe8b5e5268
Saurabh Dantani <saurabh@futurestacksolution.in>
2024-09-03 16:31:58 +0530

    chnage

---
+++ src/server.js
@@ -0,0 +1,17 @@
+// src/server.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.static('public')); // Serving static files
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
